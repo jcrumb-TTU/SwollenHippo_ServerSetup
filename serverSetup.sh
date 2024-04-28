bdefd7d47254de90ce743a492f03cbbf61e5a742
Jacob Crumb <jcrumb42@tntech.edu>
2024-04-28 22:58:49 +0000

    Added curl to serverSetup.sh

--- serverSetup.sh
+++ serverSetup.sh
@@ -4,3 +4,13 @@
 #Creation date:
 #
 #Description:
+
+#strIP=$1
+
+#strTicketID=$2
+
+strURL="https://www.swollenhippo.com/ServiceNow/systems/devTickets.php"
+
+arrResult=$(curl ${strURL})
+
+echo ${arrResult}
