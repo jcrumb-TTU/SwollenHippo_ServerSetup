810155266bfa647461a58faccce19586215fb23e
Jacob Crumb <jcrumb42@tntech.edu>
2024-04-28 22:54:44 +0000

    Copied copy.sh script from project document

---
+++ copy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Description: Copies and runs automation script on GCP remote server
+# Author: Ben Burchfield
+# Date: 17 April 2024
+strIP=$1
+strTicketID=$2
+strUsername=$3
+eval "$(ssh-agent -s)"
+# The location of my gcp ssh key is in the .ssh directory
+# My gcp ssh key is called gcp ssh-add .ssh/gcp
+# Using scp, copies a shell script called serverSetup.sh from the
+# current directory to the home directory on the remote server
+# Note the two lines below is actually one line
+scp -i .ssh/gcp serverSetup.sh "${strUsername}"@"${strIP}":/home/"${strUsername}"
+
+ssh ${strUsername}@${strIP} "chmod 755 serverSetup.sh"
+ssh ${strUsername}@${strIP} "./serverSetup.sh ${strIP} ${strTicketID}
