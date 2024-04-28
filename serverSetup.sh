2504498d72a7290e4881f5be27e11e4d8846269d
Jacob Crumb <jcrumb42@tntech.edu>
2024-04-28 23:03:11 +0000

    Added outer while loop for json array

--- serverSetup.sh
+++ serverSetup.sh
@@ -14,3 +14,18 @@ strURL="https://www.swollenhippo.com/ServiceNow/systems/devTickets.php"
 arrResult=$(curl ${strURL})
 
 echo ${arrResult}
+
+intLength=$(echo ${arrResult} | jq 'length'
+
+intCurrent=0
+
+while [ "$intCurrent" -lt "$intLength" ];do
+
+   strJSONTicket=$(echo ${arrResult} | jq -r .[intCurrent].ticketID)
+
+
+   if
+
+
+((intCurrent++))
+done
