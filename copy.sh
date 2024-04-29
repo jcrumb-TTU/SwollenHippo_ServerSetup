f40c7a37e45f3fa9c396109b46677bb24dbde5cc
Jacob Crumb <jcrumb42@tntech.edu>
2024-04-29 18:06:13 +0000

    Copy.sh modified for my ssh key

--- copy.sh
+++ copy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Description: Copies and runs automation script on GCP remote server
-# Author: Ben Burchfield
+# Author: Ben Burchfield, with slight modifications by Jacob Crumb
 # Date: 17 April 2024
 strIP=$1
 strTicketID=$2
 strUsername=$3
 eval "$(ssh-agent -s)"
 # The location of my gcp ssh key is in the .ssh directory
-# My gcp ssh key is called gcp ssh-add .ssh/gcp
+# My gcp ssh key is called gcp
+ssh-add ~/.ssh/gcpserver
 # Using scp, copies a shell script called serverSetup.sh from the
 # current directory to the home directory on the remote server
 # Note the two lines below is actually one line
